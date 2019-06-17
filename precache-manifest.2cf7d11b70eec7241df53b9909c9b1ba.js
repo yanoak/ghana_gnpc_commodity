196ef1cace665ff0d7f5d0fdf1b89499b1269bf5
Yan <yan@Yans-MacBook-Pro.local>
2019-06-17 12:20:29 +0630

    Updates

---
+++ precache-manifest.2cf7d11b70eec7241df53b9909c9b1ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05911317ab0f4476b3e2b8829ad05eda",
+    "url": "/ghana_gnpc_commodity/index.html"
+  },
+  {
+    "revision": "59720fab17b84433c7f2",
+    "url": "/ghana_gnpc_commodity/static/css/main.70c330c8.chunk.css"
+  },
+  {
+    "revision": "b05690f4311d12b267ea",
+    "url": "/ghana_gnpc_commodity/static/js/2.5754a245.chunk.js"
+  },
+  {
+    "revision": "59720fab17b84433c7f2",
+    "url": "/ghana_gnpc_commodity/static/js/main.9eb926fe.chunk.js"
+  },
+  {
+    "revision": "4b98e433b7b5bdb3f21d",
+    "url": "/ghana_gnpc_commodity/static/js/runtime~main.a9f880e4.js"
+  },
+  {
+    "revision": "97d6266204339226f73379042d090b3f",
+    "url": "/ghana_gnpc_commodity/static/media/gnpc_commodity_exports.97d62662.csv"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ghana_gnpc_commodity/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/ghana_gnpc_commodity/precache-manifest.2cf7d11b70eec7241df53b9909c9b1ba.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ghana_gnpc_commodity/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
