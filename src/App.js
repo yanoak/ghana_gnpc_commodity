838520a96e6e1d0ba51e55c5239f030848dad24c
Yan <yan@Yans-MacBook-Pro.local>
2019-06-26 01:03:48 +0800

    revenue as circles, toggle timeline, filter years, responsive

--- src/App.js
+++ src/App.js
@@ -1,27 +1,77 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import 'react-widgets/dist/css/react-widgets.css';
 import _ from 'lodash'
 import { loadAllData } from './DataLoading';
 import MainVizComponent from './components/MainVizComponent/MainVizComponent';
+import {Helmet} from "react-helmet";
+import Switch from "react-switch";
+import { Combobox } from 'react-widgets';
 
 class App extends Component {
 
-  state = {
-    ...this.props,
-    data: []
+  constructor() {
+    super()
+    this.state = {
+      ...this.props,
+      dimensions: {
+        width: window.innerWidth,
+        height: window.innerHeight
+      },
+      data: [],
+      timeline: false,
+      revAsCircles: true
+    };
+    this.handleTimeLineChange = this.handleTimeLineChange.bind(this)
+    this.handleFilterYearChange = this.handleFilterYearChange.bind(this)
+    this.handleRevAsCirclesChange = this.handleRevAsCirclesChange.bind(this)
+    this.updateDimensions = this.updateDimensions.bind(this)
   }
 
+  
+
   componentDidMount() {
-    console.log('mounted')
+    window.addEventListener("resize", this.updateDimensions);
     this.getData();
+    console.log('mounted')
+  }
+
+  updateDimensions() {
+    this.setState({
+      dimensions: {
+				height: window.innerHeight, 
+				width: window.innerWidth
+			}
+    });
+  }
+
+  handleTimeLineChange(checked) {
+    this.setState({ timeline: checked });
+  }
+
+  handleRevAsCirclesChange(checked) {
+    this.setState({ revAsCircles: checked });
   }
   
+  handleFilterYearChange(year) {
+    console.log(year);
+    const filteredData = year === "All"
+      ? this.state.data
+      : this.state.data.filter(d => d.Date_of_sale.year() === +year)
+    this.setState({ filteredData });
+  }
+
   getData() {
     loadAllData()
       .then(data => {
         // set initial data
-        this.setState({ data });
+        var years = ["All", ..._.uniq(data.map(d => d.Date_of_sale.year()))];
+        this.setState({ 
+          data: data, 
+          filteredData: data,
+          years: years
+        });
       })
   }
 
@@ -31,14 +81,51 @@ class App extends Component {
     const isLoading = !!(_.isEmpty(this.state.data)) ? true : false;
 
     return (
+
+      
       !!isLoading
       ? null
       : <div className="App">
+
         <header>
+        <div className="application">
+            <Helmet>
+              <meta charSet="utf-8" />
+              <meta name="viewport" content="width=1100,initial-scale=0.3,shrink-to-fit=yes" />
+            </Helmet>
+        </div>
         </header>
+
+        <div className="uiWidgetContainer">
+          <label>
+            <span>Revenue as Circles</span>
+            <br/>
+            <Switch onChange={this.handleRevAsCirclesChange} checked={this.state.revAsCircles} />
+          </label>
+        </div>
+        <div className="uiWidgetContainer">
+          <label>
+            <span>Timeline View</span>
+            <br/>
+            <Switch onChange={this.handleTimeLineChange} checked={this.state.timeline} />
+          </label>
+        </div>
+        <div className="uiWidgetContainer">
+          Filter by year:
+          <Combobox
+            data={this.state.years}
+            defaultValue={"All"}
+            onChange={this.handleFilterYearChange }
+          />
+        </div>
+        
+
         <div className="vizContainer">
           <MainVizComponent
-            data = {this.state.data}
+            data = {this.state.filteredData}
+            width = {this.state.dimensions.width*.95}
+            timeline = {this.state.timeline}
+            revAsCircles = {this.state.revAsCircles}
           />
         </div>
         
--- src/components/MainVizComponent/MainVizComponent.js
+++ src/components/MainVizComponent/MainVizComponent.js
@@ -10,50 +10,78 @@ import { format } from 'd3-format'
 import { default as tip } from 'd3-tip'
 import { min, max } from 'd3-array'
 import { makeTooltip } from '../../VizHelpers'
+import * as moment from 'moment'
+
 
 
 class MainVizComponent extends Component {
 	constructor(props) {
 		super(props)
-		this.createChart = this.createChart.bind(this)
+    this.createChart = this.createChart.bind(this)
+    this.state = {
+      height: 4000
+    }
 	}
 	componentDidMount() {
+    this.setState({
+      height: this.props.data.length * 180
+    })
 		this.createChart()
 	}
 	
 	componentDidUpdate() {
 		this.createChart()
-	}
+  }
+  
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.data !== this.props.data) {
+      this.setState({
+        height: nextProps.data.length * 180
+      })
+    }
+  }
 
 	createChart() {
 		let data = this.props.data ? this.props.data : [];
-		var margin = {top: 150, right: 5, bottom: 5, left: 450},
-		width = 1000 - margin.left - margin.right,
-		height = 4000 - margin.top - margin.bottom,
+    // let timeline = this.props.timeline;
+    let {revAsCircles, timeline} = this.props;
+		var margin = {top: 150, right: 5, bottom: 5, left: this.props.width/3 },
+		width = this.props.width - margin.left - margin.right,
+		height = this.state.height - margin.top - margin.bottom,
 		node = select(this.node);
 
 		console.log(data);
+		console.log(revAsCircles);
 
 		data = _.sortBy(data,"Date_of_sale")
-		// const barHeight = 5;
+    // const barHeight = 5;
+    data.forEach(d => {
+      d.yRevBottom = 0;
+      d.yRevTop = 0;
+    })
 
 		// var durationFormat = format(".1f");
 		var numFormat = format(',.2f');
 
-		var y1 = scaleLinear()
+		var yTimeLine = scaleLinear()
+      .range([height, 0]);
+      
+    var yIndex = scaleLinear()
     	.range([height, 0]);
 
 		var config = {
       xOffset: 0,
       yOffset: 0,
-      width: 150,
+      // width: 150,
+      width: width/8,
       height: height,
       labelPositioning: {
         alpha: 0.5,
         spacing: 18
       },
       leftTitle: "Lifting",
-      rightTitle: "Buyer & Destination",
+      rightTitle1: "Buyer",
+      rightTitle2: "& Destination",
       leftSubTitle: "Date of Sale",
       rightSubTitle: "Payment Due Date",
       labelGroupOffset: 5,
@@ -63,27 +91,31 @@ class MainVizComponent extends Component {
       // Reduce this to turn on detail-on-hover version
 			unfocusOpacity: 0.3,
 			revWidthMultiple: 3.5,
-			priceWidthMultiple: 1.5
+      priceWidthMultiple: 1.5,
+      timelineWidthMultiple: 0.6
 		}
 		
 		var smallConfig = {
-			maxwidth: 50,
+			maxwidth: width/15 > 20 ? width/15 : 20,
 			height: 10,
 			// color: '#c59800',
 			color: '#ecb600',
 			highlightColor: '#CE1126',
 			border: '#eee',
-			volWidth: 3,
+      volWidth: 2.5,
+      revBoxesPerCol: 10,
 			maxRevWidth: 400,
 			volTitle: "Volume (barrels)",
-			priceTitle: "Price (US$)",
-			revTitle: "Revenue (US$)",
+			priceTitle1: "Price",
+			priceTitle2: "(US$)",
+			revTitle1: "Revenue",
+			revTitle2: "(US$)",
 		}
 
 		let tooltip = tip()
 			.attr('class', 'd3-tip')
 			.offset([0, 20])
-			.direction(d => 'se')
+			.direction(d => revAsCircles ? 'e':'se')
 			.html((d) => {
 				return makeTooltip(
 					d.year,
@@ -101,34 +133,32 @@ class MainVizComponent extends Component {
 			})
 
 		node.selectAll("g").remove();
-		// set the ranges
-		// var x = scaleBand()
-		//   .range([0, width])
-		//   .domain([2013,2014,2015,2016,2017,2018])
-		//   .padding(0.1);
-		var x = scaleLinear()
-			.range([0, width])
-			// .domain([0,_.max(this.props.maxDuration)]);
-			// .domain([0,_.max(durations)]); 
-			.domain([0,3600*24.0]); 
 
 		var colorScale = scaleSequential(interpolateReds)
-		// .domain([0,_.max(cpues)])
 		.domain([0,10])
-		// .domain([0,5,10,15,20,25,30,35,40])
-		// .range(schemeBlues);
 
 		var getColor = function(cpue) {
 			if (cpue) {
 				return colorScale(cpue);
 			} else return "#ccc";
-		}
+    }
+    
+    var getY = function(yVal,index) {
+      if (timeline) {
+        return yTimeLine(yVal);
+      } else {
+        return yIndex(index);
+      }
+    }
 
 			 
-		// Calculate y domain for ratios
-		y1.domain([new Date(2018, 3, 30), new Date(2015, 0, 1)]);
+    // Calculate y domain for ratios
+    var maxDate = moment(moment.max(data.map(d => d.Payment_receipt_date)).toDate());
+    var minDate = moment(moment.min(data.map(d => d.Date_of_sale)).toDate());
+
+		yTimeLine.domain([maxDate.add(2, 'months').toDate(), minDate.subtract(1, 'months').toDate()]);
+		yIndex.domain([data.length, -1]);
 		
-		var yScale = y1;
 				
 		var borderLines = node.append("g")
 				.attr("class", "border-lines")
@@ -137,8 +167,8 @@ class MainVizComponent extends Component {
 			.attr("x1", 0).attr("y1", 0)
 			.attr("x2", 0).attr("y2", config.height);
 		borderLines.append("line")
-			.attr("x1", config.width ).attr("y1", 0)
-			.attr("x2", config.width ).attr("y2", config.height);
+			.attr("x1", config.width*config.timelineWidthMultiple ).attr("y1", 0)
+			.attr("x2", config.width*config.timelineWidthMultiple ).attr("y2", config.height);
 		
 		var slopeGroups = node.append("g")
 			.selectAll("g")
@@ -155,24 +185,24 @@ class MainVizComponent extends Component {
 		var slopeLines = slopeGroups.append("line")
 			.attr("class", "slope-line")
 			.attr("x1", 0)
-			.attr("y1", function(d) {
-				console.log(d);
-				return y1(d.sellDate);
+			.attr("y1", function(d,i) {
+				return getY(d.sellDate,i);
 			})
-			.attr("x2", config.width)
-			.attr("y2", function(d) {
-				return y1(d.paymentDate);
+			.attr("x2", config.width*config.timelineWidthMultiple)
+			.attr("y2", function(d,i) {
+				return getY(d.paymentDate,i);
 			});
 		
 		var leftSlopeCircle = slopeGroups.append("circle")
+      .attr("class", function(d) { return "circle";})
 			.attr("r", config.radius)
-			.attr("cy", d => y1(d.sellDate));
+			.attr("cy", function(d,i) { return getY(d.sellDate,i);});
 		
 		var leftSlopeLabels = slopeGroups.append("g")
 			.attr("class", "slope-label-left")
-			.each(function(d) {
+			.each(function(d,i) {
 				d.xLeftPosition = -config.labelGroupOffset;
-				d.yLeftPosition = y1(d.sellDate);
+				d.yLeftPosition = getY(d.sellDate,i);
 			});
 		
 		leftSlopeLabels.append("text")
@@ -193,16 +223,17 @@ class MainVizComponent extends Component {
 			.attr("text-anchor", "end")
 			.text(d => d.Lifting);
 		
-		var rightSlopeCircle = slopeGroups.append("circle")
-			.attr("r", config.radius)
-			.attr("cx", config.width)
-			.attr("cy", d => y1(d.paymentDate));
+    var rightSlopeCircle = slopeGroups.append("circle")
+      .attr("class", function(d) { return "circle";})
+    	.attr("r", config.radius)
+			.attr("cx", config.width*config.timelineWidthMultiple)
+			.attr("cy", function(d,i) { return getY(d.paymentDate,i);});
 		
 		var rightSlopeLabels = slopeGroups.append("g")
 			.attr("class", "slope-label-right")
-			.each(function(d) {
-				d.xRightPosition = config.width + config.labelGroupOffset;
-				d.yRightPosition = y1(d.paymentDate);
+			.each(function(d,i) {
+				d.xRightPosition = config.width*config.timelineWidthMultiple + config.labelGroupOffset;
+				d.yRightPosition = getY(d.paymentDate,i);
 			});
 		
 		rightSlopeLabels.append("text")
@@ -210,7 +241,7 @@ class MainVizComponent extends Component {
 			.attr("x", d => d.xRightPosition)
 			.attr("y", d => d.yRightPosition)
 			.attr("dx", 10)
-			.attr("dy", config.subTitleYShift)
+			.attr("dy", 2*config.subTitleYShift)
 			.attr("text-anchor", "start")
 			.text(d => (d.Payment_receipt_date.format("D MMM YYYY")));
 		
@@ -221,7 +252,16 @@ class MainVizComponent extends Component {
 			.attr("dx", 10)
 			.attr("dy", 3)
 			.attr("text-anchor", "start")
-			.text(d => d.Buyer +", " +d.Destination);
+      .text(d => d.Buyer);
+      
+    rightSlopeLabels.append("text")
+			.attr("class", "label-figure")
+			.attr("x", d => d.xRightPosition)
+			.attr("y", d => d.yRightPosition)
+			.attr("dx", 10)
+			.attr("dy", 3+config.subTitleYShift)
+			.attr("text-anchor", "start")
+			.text(d => d.Destination);
       
 		var titles = node.append("g")
 			.attr("transform", "translate(" + 0 + "," + margin.top + ")")
@@ -242,15 +282,20 @@ class MainVizComponent extends Component {
 			.text(config.leftSubTitle);
 		
 		titles.append("text")
-			.attr("x", margin.left + config.width)
+			.attr("x", margin.left + config.width*config.timelineWidthMultiple)
 			.attr("dx", 10)
 			.attr("dy", -margin.top / 4)
-			.text(config.rightTitle);
-		titles.append("text")
-			.attr("class", "label-figure-small")
-			.attr("x", margin.left + config.width)
+      .text(config.rightTitle1);
+    titles.append("text")
+			.attr("x", margin.left + config.width*config.timelineWidthMultiple)
 			.attr("dx", 10)
 			.attr("dy", -margin.top / 4 + config.subTitleYShift)
+			.text(config.rightTitle2);
+		titles.append("text")
+			.attr("class", "label-figure-small")
+			.attr("x", margin.left + config.width*config.timelineWidthMultiple)
+			.attr("dx", 10)
+			.attr("dy", -margin.top / 4 + 2*config.subTitleYShift)
 			.text(config.rightSubTitle);
 
 
@@ -261,11 +306,20 @@ class MainVizComponent extends Component {
 
 		var smallX = scaleLinear()
 			.range([0, smallConfig.maxwidth])
-			.domain([0,70]); 
+      .domain([0,100]); 
+      
+    var maxRev = max(data.map(d => d.Revenue))
+    var circleX = scaleLinear()
+			.range([0, config.width])
+			.domain([0,Math.sqrt(maxRev)]); 
 
 		function makeClass(d) {
 			return d.Lifting.split(" ").join("").toLowerCase();
 		} 
+
+    var volBars = [];
+		var revBars = [];
+		var annotations = [];
 
 		priceBars.selectAll(".bar")
       .data(data)
@@ -293,13 +347,57 @@ class MainVizComponent extends Component {
 				selectAll(".annote."+ makeClass(d))
 					.attr("display", "none")
       })
+
+      if (revAsCircles) {
+        revBars.push(node.append("g")
+          .attr("transform", "translate(" + (margin.left + config.width*config.revWidthMultiple) + "," + margin.top + ")")
+          .attr("class", function(d) { return "rev";}));
+          
+        revBars[0].selectAll(".bar")
+          .data(data)
+        .enter().append("circle")
+          .attr("class", function(d) { return "bar rev " + makeClass(d);})
+          .attr("cx", function(d) { 
+            return config.width;
+          })
+          .attr("r", function(d) { return circleX(Math.sqrt(+d.Revenue)) })
+          .attr("cy", function(d) { 
+
+            var returnVal = d.yRightPosition;
+            
+            return returnVal; 
+          })
+          .attr("fill", function(d) { return smallConfig.color; })
+          .attr("stroke", function(d) { return smallConfig.border; })
+          // .attr("height", function(d) { return smallConfig.height; })
+          .on('mouseover', function(d) {
+            tooltip.show(d,this);
+            selectAll("."+makeClass(d))
+              .attr("fill", smallConfig.highlightColor);
+            selectAll(".annote."+ makeClass(d))
+              .attr("display", "block")
+          })
+          .on('mouseout', function(d) {
+            tooltip.hide();
+            selectAll("."+makeClass(d))
+              .attr("fill", smallConfig.color);
+            selectAll(".annote."+ makeClass(d))
+              .attr("display", "none")
+          })
+      } 
 			
 		titles.append("text")
 			.attr("text-anchor", "start")
 			.attr("x", margin.left + config.width*(config.priceWidthMultiple+1))
 			.attr("dx", -10)
 			.attr("dy", -margin.top / 4)
-			.text(smallConfig.priceTitle);
+			.text(smallConfig.priceTitle1);
+    titles.append("text")
+			.attr("text-anchor", "start")
+      .attr("x", margin.left + config.width*(config.priceWidthMultiple+1))
+      .attr("dx", -10)
+      .attr("dy", -margin.top / 4 + config.subTitleYShift)
+      .text(smallConfig.priceTitle2);
 
 		titles.append("text")
 			.attr("text-anchor", "start")
@@ -313,7 +411,12 @@ class MainVizComponent extends Component {
 			.attr("x", margin.left + config.width*config.revWidthMultiple)
 			.attr("dx", 0)
 			.attr("dy", -margin.top / 4)
-			.text(smallConfig.revTitle);
+			.text(smallConfig.revTitle1);
+    titles.append("text")
+      .attr("x", margin.left + config.width*config.revWidthMultiple)
+      .attr("dx", 0)
+      .attr("dy", -margin.top / 4 + config.subTitleYShift)
+      .text(smallConfig.revTitle2);
 
 		var volData = data.map(function(d) {
 			return _.range(0,Math.round(d.Volume/10000))
@@ -322,10 +425,7 @@ class MainVizComponent extends Component {
 
 		console.log(volData);
 
-		var volBars = [];
-		var revBars = [];
-		var annotations = [];
-
+		
 		data.forEach(function(currentBatch,i) {
 			volBars.push(node.append("g")
 				.attr("transform", "translate(" + 10 + "," + margin.top + ")")
@@ -364,46 +464,57 @@ class MainVizComponent extends Component {
 						.attr("display", "none")
 				})
 
-			revBars[i].selectAll(".bar")
-				.data(volData[i])
-			.enter().append("rect")
-				.attr("class", function(d) { return "bar rev " + makeClass(currentBatch);})
-				.attr("x", function(d,j) { 
-					var boxWidth = smallX(+currentBatch.Price)
-					var boxesPerRow = Math.floor(smallConfig.maxRevWidth/boxWidth);
-					var index = j % boxesPerRow;
-					return index*boxWidth;
-				})
-				.attr("width", function(d) { return smallX(+currentBatch.Price) })
-				.attr("y", function(d,j) { 
-					var boxWidth = smallX(+currentBatch.Price)
-					var boxesPerRow = Math.floor(smallConfig.maxRevWidth/boxWidth);
-					var row = Math.floor(j/boxesPerRow);
+      if (!revAsCircles) {
+        revBars[i].selectAll(".bar")
+          .data(volData[i])
+        .enter().append("rect")
+          .attr("class", function(d) { return "bar rev " + makeClass(currentBatch);})
+          .attr("x", function(d,j) { 
+            var boxWidth = smallX(+currentBatch.Price)
+            // var boxesPerRow = Math.floor(smallConfig.maxRevWidth/boxWidth);
+            var index = Math.floor(j / smallConfig.revBoxesPerCol);
+            return index*boxWidth;
+            
 
-					var returnVal = currentBatch.yRightPosition - smallConfig.height/2 + (smallConfig.height*row);
-					data[i].yRevBottom = data[i].yRevBottom > returnVal + smallConfig.height 
-						? data[i].yRevBottom
-						: returnVal + smallConfig.height;
-					
-					return returnVal; 
-				})
-				.attr("fill", function(d) { return smallConfig.color; })
-				.attr("stroke", function(d) { return smallConfig.border; })
-				.attr("height", function(d) { return smallConfig.height; })
-				.on('mouseover', function(d) {
-					tooltip.show(currentBatch,this);
-					selectAll("."+makeClass(currentBatch))
-						.attr("fill", smallConfig.highlightColor);
-					selectAll(".annote."+ makeClass(currentBatch))
-						.attr("display", "block")
-				})
-				.on('mouseout', function(d) {
-					tooltip.hide();
-					selectAll("."+makeClass(currentBatch))
-						.attr("fill", smallConfig.color);
-					selectAll(".annote."+ makeClass(currentBatch))
-						.attr("display", "none")
-				})
+          })
+          .attr("width", function(d) { return smallX(+currentBatch.Price) })
+          .attr("y", function(d,j) { 
+            // var boxWidth = smallX(+currentBatch.Price)
+            // var boxesPerRow = Math.floor(smallConfig.maxRevWidth/boxWidth);
+            // var row = Math.floor(j/boxesPerRow);
+
+            // data[i].yRevBottom = data[i].yRevBottom > returnVal + smallConfig.height 
+            // 	? data[i].yRevBottom
+            // 	: returnVal + smallConfig.height;
+            
+            var index = j % smallConfig.revBoxesPerCol;
+            var returnVal = currentBatch.yRightPosition - smallConfig.height/2 + (smallConfig.height*index);
+            
+            data[i].yRevBottom = data[i].yRevBottom > returnVal + smallConfig.height 
+              ? data[i].yRevBottom
+              : returnVal + smallConfig.height;
+            
+            return returnVal; 
+          })
+          .attr("fill", function(d) { return smallConfig.color; })
+          .attr("stroke", function(d) { return smallConfig.border; })
+          .attr("height", function(d) { return smallConfig.height; })
+          .on('mouseover', function(d) {
+            tooltip.show(currentBatch,this);
+            selectAll("."+makeClass(currentBatch))
+              .attr("fill", smallConfig.highlightColor);
+            selectAll(".annote."+ makeClass(currentBatch))
+              .attr("display", "block")
+          })
+          .on('mouseout', function(d) {
+            tooltip.hide();
+            selectAll("."+makeClass(currentBatch))
+              .attr("fill", smallConfig.color);
+            selectAll(".annote."+ makeClass(currentBatch))
+              .attr("display", "none")
+          })
+      }
+			
 
 			// Volume Annotation
 			node.append("g")
@@ -438,12 +549,16 @@ class MainVizComponent extends Component {
 				.append("text")
 				.attr("text-anchor", "left")
 				.attr("class", function(d) { return "annote rev " + makeClass(currentBatch);})
-				.attr("x", margin.left + config.width*(config.revWidthMultiple+.5))
+				.attr("x", function() {
+          var defaultX = margin.left + config.width*(config.revWidthMultiple+.5);
+          return revAsCircles ? defaultX - config.width: defaultX;
+        })
 				.attr("dx", 0)
 				// .attr("y", currentBatch.yRightPosition - 10)
 				.attr("y", function(d) { 
-					data[i].yRevTop = currentBatch.yRightPosition - 10;
-					return currentBatch.yRightPosition-10;
+          data[i].yRevTop = currentBatch.yRightPosition - 30;
+          var defaultY = currentBatch.yRightPosition-10;
+          return revAsCircles ? defaultY - config.width : defaultY;
 				})
 				.attr("fill", smallConfig.color)
 				.attr("display", "none")
@@ -457,14 +572,10 @@ class MainVizComponent extends Component {
 		// Adjust Rev Blocks so they don't overlap
 		// Start from the bottom and keep pushing all the ones at the top up a bit untill
 		// they no longer overlap
-		var vCumulativeDelta = 0;
+    var vCumulativeDelta = 0;
+
 		for (var j = data.length-1; j > 0; j--) {
 			for (var i = data.length-1; i > 0; i--) {
-        console.log(data[i].Lifting,
-          data[i].yRevTop,
-          data[i-1].Lifting,
-          data[i-1].yRevBottom,
-          data[i].yRevTop-data[i-1].yRevBottom);
         
         vCumulativeDelta =  data[i-1].yRevBottom > data[i].yRevTop
           ? (data[i-1].yRevBottom-data[i].yRevTop)
@@ -473,7 +584,6 @@ class MainVizComponent extends Component {
         if (vCumulativeDelta > 0) {
           selectAll(".rev." + makeClass(data[i-1]))
           .attr("transform", "translate(" + 0 + "," + (-vCumulativeDelta) + ")")
-          // .attr("dy", vCumulativeDelta);
           
           data[i-1].yRevBottom -= vCumulativeDelta;
           data[i-1].yRevTop -= vCumulativeDelta;
@@ -493,7 +603,7 @@ class MainVizComponent extends Component {
 		return (
 			<div className="MainVizComponent">
 				<svg className="MainChart" ref={node => this.node = node}
-          width={1500} height={data.length*200}>
+          width={this.props.width} height={this.state.height}>
         </svg>
 			</div>
 		);
